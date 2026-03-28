4bca74395dbea91d25aa208b3cc0ff3836f029ce
Himanshu25102005 <himanshudusane12@gmail.com>
2026-03-28 23:56:15 +0530

    Improvise the login page

--- frontend/src/app/login/page.jsx
+++ frontend/src/app/login/page.jsx
@@ -35,9 +35,11 @@ const page = () => {
     return (
         <>
 
+
             <div className="flex h-screen bg-[#000306]">
                 <div className="border-2 border-dashed border-white h-full w-1/2">
                     <div className="h-full w-full bg-colour">
+
                         <LampContainer>
                             {/* <motion.h1
                                 initial={{ opacity: 0.5, y: 100 }}
@@ -60,7 +62,19 @@ const page = () => {
                                     ease: "easeInOut",
                                 }}
                                 className=" w-100 ">
-                                <h2 className="text-center font-semibold text-3xl text-[#DEDDDF] m-2 mb-4"> Strava </h2>
+
+                                <div className="flex">
+                                    <div >
+                                        <svg width="100" height="100" viewBox="0 0 1 00 100" fill="none" xmlns="http://www.w3.or g/2000/svg">
+                                            <path d="M20 80 L45 55 H55 L80 30" stroke="#3882F6" stroke-width="12" stroke-linecap="butt" stroke-linejoin="miter" />
+                                            <path d="M20 60 L45 35 H55 L80 10" stroke="#3882F6" stroke-width="12" stroke-opacity="0.3" stroke-linecap="butt" stroke-linejoin="miter" />
+                                            <rect x="42" y="52" width="6" height="6" f ill="#3882F6" />
+                                            <rect x="52" y="32" width="6" height="6" f il1="#3882F6" />
+                                        </svg>
+                                    </div>
+                                    <div className="text-center font-semibold text-3xl text-[#DEDDDF] m-2 mb-4"> Strava
+                                    </div>
+                                </div>
 
                                 <motion.div
 
@@ -70,36 +84,54 @@ const page = () => {
                                 </motion.div>
 
                                 <div className="px-6 mt-2 flex flex-wrap gap-2 ">
-                                    <motion.div
-                                        whileHover={{ backgroundColor: "#ffffff" }}
-                                        className="bg-[#3F3F3F] group h-13 w-full rounded-lg mt-2 flex px-5 items-center gap-3 ">
+                                    <motion.button
+                                        whileHover={{ backgroundColor: "#ffffff", scale: 1.01 }}
+                                        transition={{
+                                            delay: 0,
+                                            duration: 0.2,
+                                            ease: "easeInOut",
+                                        }}
+                                        whileTap={{ scale: 1 }}
+                                        className="bg-[#3F3F3F] cursor-pointer group h-15 w-full rounded-lg mt-2 flex px-5 items-center gap-3 ">
                                         <div className="bg-[#767676] h-6  w-6 rounded-full group-hover:bg-[#0B0B0B] text-center">
                                             <p className="text-[#DBDADA] group-hover:text-[#E1E1E1]">1</p>
                                         </div>
-                                        <div className="text-[#DBDADA] font-semibold group-hover:text-[#000000]">
+                                        <div className="text-[#DBDADA]  group-hover:text-[#000000] group-hover:font-semibold">
                                             Sign up your account
                                         </div>
-                                    </motion.div>
-                                    <motion.div
-                                        whileHover={{ backgroundColor: "#ffffff" }}
-                                        className="bg-[#3F3F3F] group h-13 w-full rounded-lg mt-2 flex px-5 items-center gap-3 ">
+                                    </motion.button>
+                                    <motion.button
+                                        whileHover={{ backgroundColor: "#ffffff", scale: 1.01 }}
+                                        transition={{
+                                            delay: 0,
+                                            duration: 0.2,
+                                            ease: "easeInOut",
+                                        }}
+                                        whileTap={{ scale: 1 }}
+                                        className="bg-[#3F3F3F] cursor-pointer group h-15 w-full rounded-lg mt-2 flex px-5 items-center gap-3 ">
                                         <div className="bg-[#767676] h-6  w-6 rounded-full group-hover:bg-[#0B0B0B] text-center">
                                             <p className="text-[#DBDADA] group-hover:text-[#E1E1E1]">2</p>
                                         </div>
-                                        <div className="text-[#DBDADA] font-semibold group-hover:text-[#000000]">
+                                        <div className="text-[#DBDADA]  group-hover:text-[#000000] group-hover:font-semibold">
                                             Answer some easy questions
                                         </div>
-                                    </motion.div>
-                                    <motion.div
-                                        whileHover={{ backgroundColor: "#ffffff" }}
-                                        className="bg-[#3F3F3F] group h-13 w-full rounded-lg mt-2 flex px-5 items-center gap-3 ">
+                                    </motion.button>
+                                    <motion.button
+                                        whileHover={{ backgroundColor: "#ffffff", scale: 1.01 }}
+                                        transition={{
+                                            delay: 0,
+                                            duration: 0.2,
+                                            ease: "easeInOut",
+                                        }}
+                                        whileTap={{ scale: 1 }}
+                                        className="bg-[#3F3F3F] cursor-pointer group h-15 w-full rounded-lg mt-2 flex px-5 items-center gap-3 ">
                                         <div className="bg-[#767676] h-6  w-6 rounded-full group-hover:bg-[#0B0B0B] text-center">
                                             <p className="text-[#DBDADA] group-hover:text-[#E1E1E1]">3</p>
                                         </div>
-                                        <div className="text-[#DBDADA] font-semibold group-hover:text-[#000000]">
+                                        <div className="text-[#DBDADA]  group-hover:text-[#000000] group-hover:font-semibold">
                                             Set up you profile
                                         </div>
-                                    </motion.div>
+                                    </motion.button>
 
 
 
