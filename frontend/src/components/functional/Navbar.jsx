1d088476e5e6087d4ff9c0c3d89a1a4c3bfee452
Himanshu25102005 <himanshudusane12@gmail.com>
2026-03-30 19:47:09 +0530

    feat: update Navbar component styling and integrate notification icon; enhance layout for improved user experience

--- frontend/src/components/functional/Navbar.jsx
+++ frontend/src/components/functional/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {IconBell} from "@tabler/icons-react";
 
 const Navbar = () => {
     return (
-        <div className='border-2 border-dashed border-white p-1 flex justify-between items-center'>
+        <div className='border-1 border-solid border-b-white p-1 flex justify-between items-center'>
 
-            <div className='border-2 border-solid border-green-900 p-2'>
+            <div className=' px-10 py-2'>
                 <svg
                     className="h-11 w-11"
                     viewBox="0 0 100 100"
@@ -15,7 +16,15 @@ const Navbar = () => {
                     <path d="M20 60 L45 35 H55 L80 10" stroke="#3882F6" strokeOpacity="0.3" strokeWidth="12" />
                 </svg>
             </div>
-            <div className='border-2 border-solid border-green-900 p-2'>
+            <div className='px-14 flex gap-10'>
+                <div>
+                    <IconBell color="#DFDFDF" size={32} />
+                </div>
+
+                <div className="border-1 border-[#DFDFDF] rounded-full h-10 w-10 bg-cover bg-center bg-no-repeat bg-[url('https://i.pinimg.com/736x/ae/a7/a9/aea7a9551cda1f88cc5e6e7ea52709f1.jpg')]">
+                </div>
+
+                
 
             </div>
 
