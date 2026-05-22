87e744fa35dfb9fbfaf836533ffaa4317e942f00
Himanshu25102005 <himanshudusane12@gmail.com>
2026-05-23 02:20:02 +0530

    Add test graphs to test the graph workings

--- frontend/src/app/test/page.jsx
+++ frontend/src/app/test/page.jsx
@@ -4,6 +4,11 @@ import { motion } from "framer-motion";
 import Card from '@/components/Common/Card';
 import { GeistSans } from 'geist/font/sans';
 import { cn } from '@/lib/utils';
+import AreaChartComponent from '@/components/Test/AreaChartComponent';
+import BarChartComponent from '@/components/Test/BarChartComponent';
+import LineChartComponent from '@/components/Test/LineChartComponent';
+LineChartComponent
+
 
 
 
@@ -11,8 +16,51 @@ const Testpage = () => {
 
     return (
         <>
-            <div className={cn(GeistSans.className, "[perspective::1000px] [transform-style:preserve-3d  ]   min-h-screen pt-16 flex justify-center items-center")}>
-                <Card />
+            <div className='h-screen w-screen flex justify-center items-center bg-neutral-900'>
+                <div className='grid grid-col-1 lg:grid-cols-3 gap-10'>
+                    <motion.div
+                        initial={{
+                            scale: 1
+                        }}
+                        whileHover={{
+                            scale: 1.1
+                        }}
+                        transition={{
+                            duration: 0.5
+                        }}
+                        className='h-100 w-100 bg-neutral-300 rounded-xl flex flex-col gap-3'>
+                        <h1 className='font-semibold text-3xl text-center'>AreaChart</h1>
+                        <AreaChartComponent />
+                    </motion.div>
+                    <motion.div
+                        initial={{
+                            scale: 1
+                        }}
+                        whileHover={{
+                            scale: 1.1
+                        }}
+                        transition={{
+                            duration: 0.5
+                        }}
+                        className='h-100 w-100 bg-neutral-300 rounded-xl flex flex-col gap-3'>
+                        <h1 className='font-semibold text-3xl text-center'>BarChart</h1>
+                        <BarChartComponent />
+                    </motion.div>
+                    <motion.div
+                        initial={{
+                            scale: 1
+                        }}
+                        whileHover={{
+                            scale: 1.1
+                        }}
+                        transition={{
+                            duration: 0.5
+                        }}
+                        className='h-100 w-100 bg-neutral-300 rounded-xl flex flex-col gap-3'>
+                        <h1 className='font-semibold text-3xl text-center'>LineChart</h1>
+                        <LineChartComponent />
+                    </motion.div>
+                </div>
             </div>
         </>
     )
---
+++ frontend/src/components/Test/AreaChartComponent.jsx
@@ -0,0 +1,100 @@
+'use client'
+import { AreaChart, Area, ResponsiveContainer, XAxis, YAxis, CartesianGrid, Legend, Tooltip } from 'recharts'
+import React from 'react'
+
+const productSales = [
+  {
+    name: 'Jan',
+    product1: 4000,
+    product2: 2400
+  },
+  {
+    name: 'Feb',
+    product1: 3500,
+    product2: 2800
+  },
+  {
+    name: 'Mar',
+    product1: 4200,
+    product2: 3100
+  },
+  {
+    name: 'Apr',
+    product1: 4800,
+    product2: 2900
+  },
+  {
+    name: 'May',
+    product1: 5100,
+    product2: 3400
+  },
+  {
+    name: 'Jun',
+    product1: 4600,
+    product2: 4100
+  },
+  {
+    name: 'Jul',
+    product1: 4300,
+    product2: 3900
+  },
+  {
+    name: 'Aug',
+    product1: 4900,
+    product2: 4300
+  },
+  {
+    name: 'Sep',
+    product1: 5500,
+    product2: 4700
+  },
+  {
+    name: 'Oct',
+    product1: 6200,
+    product2: 5200
+  },
+  {
+    name: 'Nov',
+    product1: 7500,
+    product2: 6100
+  },
+  {
+    name: 'Dec',
+    product1: 8900,
+    product2: 7400
+  }
+];
+
+const AreaChartComponent = () => {
+  return (
+    <ResponsiveContainer height="100%" width="100%">
+      <AreaChart width={200} height={200} data={productSales} margin={{right:30, top:10}}>
+      <YAxis/>
+      <XAxis dataKey="name" />
+      <CartesianGrid strokeDasharray="5,5" stroke='#111111'/>
+      <Legend/>
+      <Tooltip/>
+        <Area
+          type="monotone"
+          stroke='#111111'
+          fill='#3b82f6 '
+          dataKey="product1"
+        />
+
+        <Area
+          type="monotone"
+          stroke='#222222'
+          fill='#3b82f0 '
+          dataKey="product2"
+        />
+      </AreaChart>
+    </ResponsiveContainer>
+  )
+}
+
+
+
+export default AreaChartComponent
+
+
+
